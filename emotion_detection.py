35e72aee4499e89b109363bf6f3d89a48eae9cef
GMSTER22 <gmster22@gmail.com>
2025-07-12 22:56:17 -0400

    format the output

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def emotion_detector(text):
     text_to_analyse = text
@@ -6,4 +7,16 @@ def emotion_detector(text):
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     input_dict = { "raw_document": { "text": text_to_analyse } }
     res = requests.post( url, json = input_dict, headers=headers )
-    return res.text
+    formatted_response = json.loads(res.text)
+    
+    output = formatted_response['emotionPredictions'][0]['emotion']
+
+    dominant_emotion_name = '' 
+    dominant_emotion_value = 0 
+
+    for emotion in output:
+        if output[emotion] > dominant_emotion_value:
+            dominant_emotion_name = emotion 
+            dominant_emotion_value = output[emotion]
+    output['dominant_emotion'] = dominant_emotion_name
+    print(output)
